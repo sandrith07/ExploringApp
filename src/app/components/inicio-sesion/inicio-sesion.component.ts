acb31af09ea1bcb592589d7bc5b79a19a09db95e
Gustavo-07 <gustavo-garcia1994@hotmail.com>
2020-06-05 04:22:53 -0500

    guardar evento

--- src/app/components/inicio-sesion/inicio-sesion.component.ts
+++ src/app/components/inicio-sesion/inicio-sesion.component.ts
@@ -8,9 +8,11 @@ import { AlertController } from '@ionic/angular';
   styleUrls: ['./inicio-sesion.component.scss'],
 })
 export class InicioSesionComponent implements OnInit {
-    
+  provider = new firebase.auth.GoogleAuthProvider();
   constructor(public alertController: AlertController) {
+
     this.comprobarSesion();
+    
    }
   accion = 'iniciar';
 
@@ -58,8 +60,71 @@ export class InicioSesionComponent implements OnInit {
     }    
   }
 
-  registroConGoogle(){
-    
+  registroConGoogleInterno(){
+    firebase.auth().signInWithPopup(this.provider).then(function (result) {
+      // This gives you a Google Access Token. You can use it to access the Google API.
+      var token = result.credential
+      // The signed-in user info.
+      var user = result.user;
+      console.log('usuario logueado ', user);
+
+      //guardar usuario en la base de datos 
+
+      firebase.database().ref('usuarios/'+user.uid).set({
+        nombre: user.displayName,
+        correo: user.email,
+        
+      })
+      
+      // ...
+    }).catch(function(error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // The email of the user's account used.
+      var email = error.email;
+      // The firebase.auth.AuthCredential type that was used.
+      var credential = error.credential;
+      // ...
+      console.log('error con google interno ', error);
+
+    });
+  }
+
+  registroConGoogleExterno(){
+
+    firebase.auth().signInWithRedirect(this.provider);
+
+    firebase.auth().getRedirectResult().then((result)=> {
+      if (result.credential) {
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        var token = result.credential;
+        // ...
+      }
+      // The signed-in user info.
+
+
+      var user = result.user;
+
+      console.log('usuario logueado ', user);
+
+      //guardar usuario en la base de datos 
+
+      firebase.database().ref('usuarios/'+user.uid).set({
+        nombre: user.displayName,
+        correo: user.email,
+        
+      })
+    }).catch(function(error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // The email of the user's account used.
+      var email = error.email;
+      // The firebase.auth.AuthCredential type that was used.
+      var credential = error.credential;
+      // ...
+    });
   }
 
 
--- src/app/pages/registrar-eventos/registrar-eventos.page.ts
+++ src/app/pages/registrar-eventos/registrar-eventos.page.ts
@@ -41,14 +41,22 @@ export class RegistrarEventosPage implements OnInit {
   direccion
   fechainicio
   fechafin
-  
+  keyEvento
   registrarEventos(){
-    firebase.database().ref('/eventos').push({
+    
+    let rutaEvento = firebase.database().ref('/eventos') // ruta, tener en cuenta para consultas en tiempo real
+    this.keyEvento = rutaEvento.push().key // llave primaria de evento
+
+    rutaEvento.child(this.keyEvento).set({ /// ruta -> '/eventos/-kfbakjbasdkb/{datos}'
     "nombre": this.nombre,
     "descripcion": this.descripcion,
     "direccion": this.direccion,
     "fechainicio": this.fechainicio,
     "fechafin": this.fechafin,
+    }).then(async ()=>{
+          //llamar a guardar archivo
+          await this.subirArchivo()
+
     }).catch((error) =>{
       console.log('ocurrio un error al intentar crear un evento =>',error);
       this.alertEventoNoGuardado()
@@ -90,4 +98,123 @@ export class RegistrarEventosPage implements OnInit {
 
     await alert.present();
   }
+
+  imagenlocal: string = "./assets/add-image.jpeg";
+  imagenSubida: File = null;
+
+  entradaDeArchivo(file: FileList) {
+    this.imagenSubida = file.item(0);
+
+    // vista de imagen previa
+    let reader = new FileReader();
+    reader.onload = (event: any) => {
+      this.imagenlocal = event.target.result;
+    };
+    reader.readAsDataURL(this.imagenSubida);
+  }
+
+
+  archivo
+  nombreArchivo
+  rutaArchivo
+  
+  seleccionarArchivo(files){
+    if(files.length === 0) // si no selecciona nada 
+      return
+    
+      let reader = new FileReader();
+      this.archivo = files
+      this.nombreArchivo = files[0].name
+
+      reader.readAsDataURL(files[0]);
+
+      reader.onload = ()=>{
+        this.rutaArchivo = reader.result
+        console.log(' ruta archivo => ',this.rutaArchivo);
+        console.log('archivo ->' , this.archivo);
+        //this.subirArchivo()
+      }
+
+
+  }
+
+  //fin seleccionar archivo -------------------------------------
+
+  urlDescargaArchivo
+  async subirArchivo(){
+    /**
+     * 0. registrar evento en database
+     * 1. obtener el id del evento
+     * 2. crear la tarea para subir el archivo
+     * 3. obtener la ruta del archivo subido
+     * 4. actualizar la info del evento en la base de datos
+     */
+
+    let extension = this.archivo[0].type.split('/').slice(-1)[0];
+      console.log('extension ', extension);
+
+    let tareaSubirArchivo =  firebase.storage().ref('/imagenes/eventos/'+this.keyEvento+'/perfil.'+extension).putString(this.rutaArchivo, 'data_url');
+
+    await tareaSubirArchivo.on('state_changed',(progreso)=>{
+      // codigo a utilizar con la carga
+      this.pro = (progreso.bytesTransferred / progreso.totalBytes) * 100
+ 
+    },(erro)=>{
+      console.log('ocurrio un error al cargar e larchivo ', this.nombreArchivo);
+      console.log('detalle error ', erro);
+    },()=>{
+      // obtener la url del archivo subido
+     // this.urlDescargaArchivo = tareaSubirArchivo.snapshot.downloadURL
+
+      tareaSubirArchivo.snapshot.ref.getDownloadURL().then((downloadURL) =>{
+       this.urlDescargaArchivo = downloadURL
+
+        //console.log('File available at', downloadURL);
+      console.log('ruta urll imagne ', this.urlDescargaArchivo );
+
+        /**
+         * 4.1 actualizar evento en la base de datos ... atributo ruta imagen
+         * 4.2 actualizar evento en la base de datos con otro metodo... escritura en multiples rutas
+         * 
+         */
+
+         //4.1 inicio ----------------------
+
+         firebase.database().ref('eventos/'+this.keyEvento+'/urlImagen').set(this.urlDescargaArchivo)
+
+         //4.2 actualizar varias rutas a la vez 
+         /* let updates = {}
+
+          updates['eventos/'+this.keyEvento+'/urlImagen'] = this.urlDescargaArchivo
+          updates['hola/'+this.keyEvento+'/urlImagen'] = this.urlDescargaArchivo
+          updates['hola2/'+this.keyEvento+'/urlImagen'] = this.urlDescargaArchivo
+          updates['hola3/'+this.keyEvento+'/urlImagen'] = this.urlDescargaArchivo
+
+
+
+
+         firebase.database().ref().update(updates).then(()=>{
+           console.log("datos actualizados correctamente");
+           
+         }).catch((erro)=>{
+           console.log('error al actualizar todas las tablas');
+           
+         })*/
+
+      });
+      
+    })
+
+
+    
+
+    
+  }
+
+  pro
+
+
+
+
+
 }
