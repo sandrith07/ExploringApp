1e1ce2a7eaf0bfaa7d0bad2e3413aeb9d9eddee8
Gustavo-07 <gustavo-garcia1994@hotmail.com>
2020-06-11 00:18:17 -0500

    Terminado

--- src/app/pages/musica/musica.page.ts
+++ src/app/pages/musica/musica.page.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFirestore, AngularFirestoreCollection} from 'angularfire2/firestore';
+import { Musica } from 'src/app/interfaces/musica.interface';
+import { ActivatedRoute} from '@angular/router';
+import { NavController, LoadingController } from '@ionic/angular';
+
+import * as firebase from 'firebase';
+import { Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-musica',
@@ -7,9 +14,69 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MusicaPage implements OnInit {
 
-  constructor() { }
+  musica: Musica = {
+    nombre: '',
+    album: '',
+    artista: '',
+    imagen: '',
+    link: '',
+    tipo: '',
+  };
+
+  musicas
+  musicasTiempoReal
+  music
+  musicaCargada: Musica[];
+  musicaClasicaFiltrada
+
+  filtro
+  musicaFiltrada
+
+  constructor() {
+    this.consultarCancionesTiempoReal(),
+    this.filtrarClasica()
+  }
 
   ngOnInit() {
+  }
+
+  keys(objeto: Object){
+    return Object.keys((objeto || {})) 
+  }
+
+  keysOrderDesc(objeto: Object){
+    return Object.keys((objeto || {})).reverse();
+  }
+
+  consultarCancionesTiempoReal(){
+    firebase.database().ref('musica').on('value', (datos)=>{
+
+      if(datos.exists()){
+        this.musicasTiempoReal = datos.val()
+        this.musicaFiltrada = Object.keys(this.musicasTiempoReal).reverse();
+      }else{
+        this.musicasTiempoReal = {}
+        this.musicaFiltrada = []
+      }
+    },(erro)=>{
+      this.musicasTiempoReal = {}
+    })
+  }
+
+  filtrarCanciones(){
+    if(!this.filtro || this.filtro == ""){
+      this.musicaFiltrada = Object.keys(this.musicasTiempoReal || {})
+    }else if(this.filtro != ""){
+      this.musicaFiltrada = this.keys(this.musicasTiempoReal).filter((musica) => (this.musicasTiempoReal[musica].nombre).toString().toLowerCase().includes((this.filtro).toString().toLowerCase())).reverse();
+
+    }
+  }
+
+  clasica = "clasica";
+  filtrarClasica(){
+    console.log('entre_al_metodo');
+    this.musicaClasicaFiltrada = this.keys(this.musicasTiempoReal).filter((musica) => (this.musicasTiempoReal[musica].tipo).toString().toLowerCase().includes((this.clasica).toString().toLowerCase())).reverse();
+    console.log('esta es la musica filtrada', this.musicaClasicaFiltrada);
   }
 
 }
